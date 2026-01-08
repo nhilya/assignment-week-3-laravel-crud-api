65e328c0cf1f93f2eacdc80b2a13791aec4ad452
nhilya <nuhailya.dev@gmail.com>
2026-01-08 22:08:27 +0800

    fix: grouping auth sanctum

--- routes/api.php
+++ routes/api.php
@@ -9,14 +9,19 @@ use Illuminate\Support\Facades\Route;
  */
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('me', [AuthController::class, 'me']);
+});
 
 /**
  * API Routes to ProductController
  */
-Route::get('products', [ProductController::class, 'index'])->middleware('auth:sanctum');
-Route::get('product/{id}', [ProductController::class, 'show'])->middleware('auth:sanctum');
-Route::post('product', [ProductController::class, 'store'])->middleware('auth:sanctum');
-Route::put('product/{id}', [ProductController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('product/{id}', [ProductController::class, 'destroy'])->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('products', [ProductController::class, 'index']);
+    Route::get('product/{id}', [ProductController::class, 'show']);
+    Route::post('product', [ProductController::class, 'store']);
+    Route::put('product/{id}', [ProductController::class, 'update']);
+    Route::delete('product/{id}', [ProductController::class, 'destroy']);
+});
