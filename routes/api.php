865cf7e69604ec4ac1aff7efaf178e640a606ada
nhilya <nuhailya.dev@gmail.com>
2026-01-08 22:34:15 +0800

    fix: grouping sanctum auth in a single group for product api

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,10 @@ Route::middleware('auth:sanctum')->group(function () {
 /**
  * API Routes to ProductController
  */
-Route::get('products', [ProductController::class, 'index']);
-Route::get('products/{id}', [ProductController::class, 'show']);
-Route::post('products', [ProductController::class, 'store']);
-Route::put('products/{id}', [ProductController::class, 'update']);
-Route::delete('products/{id}', [ProductController::class, 'destroy']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('products', [ProductController::class, 'index']);
+    Route::get('products/{id}', [ProductController::class, 'show']);
+    Route::post('products', [ProductController::class, 'store']);
+    Route::put('products/{id}', [ProductController::class, 'update']);
+    Route::delete('products/{id}', [ProductController::class, 'destroy']);
+});
